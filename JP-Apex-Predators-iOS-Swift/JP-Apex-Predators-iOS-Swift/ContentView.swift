ccd00c68a687facdaa96fc6ba6d5890fed6b6a0c
Stefano <69864405+stefanoaragoni@users.noreply.github.com>
2023-11-28 16:52:27 -0600

    Xcode Project

---
+++ JP-Apex-Predators-iOS-Swift/JP-Apex-Predators-iOS-Swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JP-Apex-Predators-iOS-Swift
+//
+//  Created by Stefano Aragoni on 28/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JP-Apex-Predators-iOS-Swift/JP-Apex-Predators-iOS-Swift/JP_Apex_Predators_iOS_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  JP_Apex_Predators_iOS_SwiftApp.swift
+//  JP-Apex-Predators-iOS-Swift
+//
+//  Created by Stefano Aragoni on 28/11/23.
+//
+
+import SwiftUI
+
+@main
+struct JP_Apex_Predators_iOS_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
