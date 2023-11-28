80244fa6d8a2c1335a24e48db59015e2357b3551
Stefano <69864405+stefanoaragoni@users.noreply.github.com>
2023-11-28 17:30:03 -0600

    L2 - ApexPredator struct / Data Structure

---
+++ JP-Apex-Predators-iOS-Swift/JP-Apex-Predators-iOS-Swift/ApexPredator.swift
@@ -0,0 +1,30 @@
+//
+//  ApexPredator.swift
+//  JP-Apex-Predators-iOS-Swift
+//
+//  Created by Stefano Aragoni on 28/11/23.
+//
+
+import Foundation
+
+
+// Codable in order to make every dinasaur in JSON an ApexPredator type
+struct ApexPredator: Codable {
+    
+    let id: Int
+    let name: String
+    let type: String
+    let movies: [String]
+    let movieScenes: [MovieScene]
+    let link: String
+    
+}
+
+// Codable in order to make every dinasaur in JSON an ApexPredator type
+struct MovieScene: Codable {
+    
+    let id: Int
+    let movie: String
+    let scene_description: String
+    
+}
